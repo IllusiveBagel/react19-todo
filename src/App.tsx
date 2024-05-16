a46f945ed6de3517cbe5e565917ac3308483c9aa
Duncan Band <band.duncan@gmail.com>
2024-05-16 13:13:24 +0100

    rm unused login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Router, Route } from "./Router";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -8,9 +7,6 @@ const App = () => {
       <Router>
         <Route href="/">
           <Home />
-        </Route>
-        <Route href="/login">
-          <Login />
         </Route>
       </Router>
     </>
